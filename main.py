07724aaf45abd6a866b53f08b8f93d47226826ef
Oliver Gauci <oliver@pycom.io>
2017-09-11 15:28:05 +0200

    Placeholder for user specific information

--- main.py
+++ main.py
@@ -3,8 +3,8 @@ from MQTT.uMQTTLib import AWSIoTMQTTClient
 import machine
 import time
 
-WIFI_SSID = 'TNCAP204295'
-WIFI_PASS = 'CC561B2F97'
+WIFI_SSID = 'Your ssid'
+WIFI_PASS = 'Your password'
 
 wlan = WLAN(mode=WLAN.STA)
 wlan.connect(WIFI_SSID, auth=(None, WIFI_PASS), timeout=5000)
@@ -20,16 +20,12 @@ def customCallback(client, userdata, message):
 	print(message.topic)
 	print("--------------\n\n")
 
-topic="pythontest"
-clientId="basicPubSub"
-host='a15n3kcirh9jxj.iot.eu-west-1.amazonaws.com'
-rootCAPath='/flash/cert/root-CA.crt'
-certificatePath='/flash/cert/WindowsSample.cert.pem'
-privateKeyPath='/flash/cert/WindowsSample.private.key'
-
-#host='a20bylp0y1bcl1.iot.us-east-2.amazonaws.com'
-#certificatePath='/flash/cert/ddd1b20d78-certificate.pem.crt'
-#privateKeyPath='/flash/cert/ddd1b20d78-private.pem.key'
+topic="your topic"
+clientId="your client id"
+host='your host'
+rootCAPath='your CA'
+certificatePath='your pubic key'
+privateKeyPath='your private key'
 
 pycomAwsMQTTClient = AWSIoTMQTTClient(clientId)
 pycomAwsMQTTClient.configureEndpoint(host, 8883)
