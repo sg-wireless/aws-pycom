11de4a9b33ec201d47ae659db4ace5041a715326
Oliver Gauci <oliver@pycom.io>
2017-09-18 12:57:53 +0200

    shadow main file

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 from network import WLAN
-from MQTT.uMQTTLib import AWSIoTMQTTClient
+from AWSIoTPythonSDK.MQTTLib import AWSIoTMQTTShadowClient
 import machine
 import time
+import json
 
-WIFI_SSID = 'Your ssid'
-WIFI_PASS = 'Your password'
+WIFI_SSID = 'your wifi ssid'
+WIFI_PASS = 'your wifi password'
 
 wlan = WLAN(mode=WLAN.STA)
 wlan.connect(WIFI_SSID, auth=(None, WIFI_PASS), timeout=5000)
@@ -13,39 +14,61 @@ while not wlan.isconnected():
 
 print('WLAN connection succeeded!')
 
-def customCallback(client, userdata, message):
-	print("Received a new message: ")
-	print(message.payload)
-	print("from topic: ")
-	print(message.topic)
-	print("--------------\n\n")
+# Custom Shadow callback
+def customShadowCallback_Update(payload, responseStatus, token):
+    # payload is a JSON string ready to be parsed using json.loads(...)
+    # in both Py2.x and Py3.x
+    if responseStatus == "timeout":
+        print("Update request " + token + " time out!")
+    if responseStatus == "accepted":
+        payloadDict = json.loads(payload)
+        print("~~~~~~~~~~~~~~~~~~~~~~~")
+        print("Update request with token: " + token + " accepted!")
+        print("property: " + str(payloadDict["state"]["desired"]["property"]))
+        print("~~~~~~~~~~~~~~~~~~~~~~~\n\n")
+    if responseStatus == "rejected":
+        print("Update request " + token + " rejected!")
 
-topic="your topic"
-clientId="your client id"
-host='your host'
-rootCAPath='your CA'
-certificatePath='your pubic key'
-privateKeyPath='your private key'
+def customShadowCallback_Delete(payload, responseStatus, token):
+    if responseStatus == "timeout":
+        print("Delete request " + token + " time out!")
+    if responseStatus == "accepted":
+        print("~~~~~~~~~~~~~~~~~~~~~~~")
+        print("Delete request with token: " + token + " accepted!")
+        print("~~~~~~~~~~~~~~~~~~~~~~~\n\n")
+    if responseStatus == "rejected":
+        print("Delete request " + token + " rejected!")
 
-pycomAwsMQTTClient = AWSIoTMQTTClient(clientId)
-pycomAwsMQTTClient.configureEndpoint(host, 8883)
-pycomAwsMQTTClient.configureCredentials(rootCAPath, privateKeyPath, certificatePath)
+thingName="your thing name"
+clientId="your client Id"
+host='your host name'
+rootCAPath='path to you root CA'
+certificatePath='path to your public key'
+privateKeyPath='path to your private key'
 
-#pycomAwsMQTTClient.configureAutoReconnectBackoffTime(1, 32, 20)
-pycomAwsMQTTClient.configureOfflinePublishQueueing(-1)  # Infinite offline Publish queueing
-pycomAwsMQTTClient.configureDrainingFrequency(2)  # Draining: 2 Hz
-pycomAwsMQTTClient.configureConnectDisconnectTimeout(5)  # 10 sec
-pycomAwsMQTTClient.configureMQTTOperationTimeout(5)  # 5 sec
-pycomAwsMQTTClient.configureLastWill(topic, 'To All: Last will message', 0)
+pycomAwsMQTTShadowClient = AWSIoTMQTTShadowClient(clientId)
+pycomAwsMQTTShadowClient.configureEndpoint(host, 8883)
+pycomAwsMQTTShadowClient.configureCredentials(rootCAPath, privateKeyPath, certificatePath)
 
-if pycomAwsMQTTClient.connect():
+# AWSIoTMQTTShadowClient configuration
+pycomAwsMQTTShadowClient.configureAutoReconnectBackoffTime(1, 32, 20)
+pycomAwsMQTTShadowClient.configureConnectDisconnectTimeout(10)  # 10 sec
+pycomAwsMQTTShadowClient.configureMQTTOperationTimeout(5)  # 5 sec
+
+# Connect to AWS IoT
+connected = pycomAwsMQTTShadowClient.connect()
+if connected:
     print('AWS connection succeeded')
 
-pycomAwsMQTTClient.subscribe(topic, 1, customCallback)
-time.sleep(2)
+deviceShadowHandler = pycomAwsMQTTShadowClient.createShadowHandlerWithName(thingName, True)
 
+# Delete shadow JSON doc
+deviceShadowHandler.shadowDelete(customShadowCallback_Delete, 5)
+
+# Update shadow in a loop
 loopCount = 0
-while loopCount < 8:
-	pycomAwsMQTTClient.publish(topic, "New Message " + str(loopCount), 1)
-	loopCount += 1
-	time.sleep(7.0)
+while True:
+    JSONPayload = '{"state":{"desired":{"property":' + str(loopCount) + '}}}'
+    deviceShadowHandler.shadowUpdate(JSONPayload, customShadowCallback_Update, 5)
+    loopCount += 1
+    time.sleep(2)
